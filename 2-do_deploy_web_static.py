11ccbc5734370de2a4ee4aba024586cc4cfaca44
Issmuth <Ismael.mdev@gmail.com>
2024-04-07 16:40:57 +0300

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,14 +9,28 @@ env.key_filename = '~/0-RSA_private_key'
 
 def do_deploy(archive_path):
     """Deploy!!!"""
-    put(archive_path, '/tmp/')
+    if os.path.isfile(archive_path) is False:
+        return False
+
+    if put(archive_path, '/tmp/').failed is True:
+        return False
+
     filename = os.path.basename(archive_path)
     file = os.path.splitext(filename)[0]
 
-    run('mkdir -p /data/web_static/releases/{}'.format(file))
-    run('tar -xvf /tmp/{} -C /data/web_static/releases/{}'
-        .format(filename, file))
-    run('rm /tmp/{}'.format(filename))
+    if run('mkdir -p /data/web_static/releases/{}'
+           .format(file)).failed is True:
+        return False
 
-    run('ln -sf /data/web_static/releases/{} /data/web_static/current'
-        .format(file))
+    if run('tar -xvf /tmp/{} -C /data/web_static/releases/{}'
+            .format(filename, file)).failed is True:
+        return False
+
+    if run('rm /tmp/{}'.format(filename)).failed is True:
+        return False
+
+    if run('ln -sf /data/web_static/releases/{} /data/web_static/current'
+            .format(file)).failed is True:
+        return False
+
+    return True
