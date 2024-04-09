e2083227f35125522bcd11443b2d951d27d5b523
Issmuth <Ismael.mdev@gmail.com>
2024-04-10 02:09:55 +0300

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
-line="server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\tadd_header X-Served-By $HOSTNAME;\n\troot   /var/www/html;\n\tindex  index.html index.htm;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tindex index.html index.htm;\n\t}\n\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=xvFZjo5PgG0&pp=ygUJcmljayByb2xs;\n\t}\n\n\terror_page 404 /404.html;\n\tlocation /404 {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}\n}"
+line="server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\tadd_header X-Served-By $HOSTNAME;\n\troot   /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\n\n\tserver_name _;\n\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tindex index.html index.htm;\n\t}\n\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=xvFZjo5PgG0&pp=ygUJcmljayByb2xs;\n\t}\n\n\terror_page 404 /404.html;\n\tlocation /404 {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}\n}"
 
 echo -e "$line" > /etc/nginx/sites-available/default
 
