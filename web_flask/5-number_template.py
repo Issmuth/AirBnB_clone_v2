e868279485db36081faee7adae0fba28343b6dd5
issmuth <ismael.mdev@gmail.com>
2024-04-20 16:15:15 +0300

    done

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """More routing and Flask intro."""
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 app = Flask(__name__)
 
@@ -36,7 +36,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return ('<body><h1>Number: {}</h1></body>'.format(n))
+    return (render_template('/5-number.html', n=n))
 
 
 if __name__ == "__main__":
