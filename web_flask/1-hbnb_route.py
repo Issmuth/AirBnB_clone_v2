714baacc39eece2d7bff2e2b7b2e92e4d54c9c4d
issmuth <ismael.mdev@gmail.com>
2024-04-20 15:07:19 +0300

    done

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,5 +10,11 @@ def hello_hbnb():
     return ("Hello HBNB!")
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns another string."""
+    return ("HBNB")
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
